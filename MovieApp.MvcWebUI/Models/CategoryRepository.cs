7e368c461c038cbed16c39c7fd3c914a4b456714
Sezer YILDIRIM <sezeryildirim@Sezer-MacBook-Pro.local>
2019-02-24 22:53:16 +0300

    Initial commit

---
+++ MovieApp.MvcWebUI/Models/CategoryRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieApp.MvcWebUI.Models
+{
+    public class CategoryRepository
+    {
+        private static List<Category> categories = null;
+        static CategoryRepository()
+        {
+            categories = new List<Category>()
+            {
+                new Category(){Id=1, Name="Category 1"},
+                new Category(){Id=2, Name="Category 2"},
+                new Category(){Id=3, Name="Category 3"},
+                new Category(){Id=4, Name="Category 4"},
+                new Category(){Id=5, Name="Category 5"},
+                new Category(){Id=6, Name="Category 6"},
+            };
+        }
+
+        public static List<Category> Categories
+        {
+            get { return categories; }
+        }
+
+        public static void AddCategory(Category category)
+        {
+            categories.Add(category);
+        }
+    }
+}
